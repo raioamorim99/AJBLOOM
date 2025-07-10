37b6ca8f2fc4636ee577b1d408baaa20ee965c8b
RaioAmorim <luan.raioamorim@icloud.com>
2025-07-10 16:33:08 -0300

    Delete AJBLOOM directory

--- AJBLOOM/backend/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // limpa as variáveis
-session_destroy(); // encerra a sessão
-header("Location: ../pages/index.php"); // redireciona pro login
-exit();
-?>
