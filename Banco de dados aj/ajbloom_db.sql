d8e62730e02eba408fe34c22cf4f9bb4618f266e
RaioAmorim <luan.raioamorim@icloud.com>
2025-07-11 20:17:05 -0300

    Add files via upload

--- Banco de dados aj/ajbloom_db.sql
+++ Banco de dados aj/ajbloom_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10/07/2025 às 21:23
+-- Tempo de geração: 12/07/2025 às 01:15
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
